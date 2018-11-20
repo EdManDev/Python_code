60e22aae745559ad31942418e889874943933bde
EdManDev <ed.manigat@gmail.com>
2018-11-20 11:49:55 -0800

    Add files via upload

--- python#4.py
+++ python#4.py
@@ -6,7 +6,7 @@ horas = float(input('ingrese la Hora: '))
 precio_por_hora = float(2.5)
 
 sub_total = horas*precio_por_hora
-descuento = sub_total*0.07
-sueldo = sub_total-descuento
 
-print('El sueldo a pagar de la hora expresado de '+valor_pagar+' es de: '+str(sueldo)+'DOP por h/mn')
+
+
+print('El sueldo a pagar de la hora expresado de '+valor_pagar+' es de: '+str(sub_total)+' DOP por h/mn')
