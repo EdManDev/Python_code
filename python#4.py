290a8e2c7dcc7c81d344f8fb3b340cfc881b4744
EdManDev <ed.manigat@gmail.com>
2018-11-20 11:56:15 -0800

    Add files via upload

--- python#4.py
+++ python#4.py
@@ -3,10 +3,10 @@ print('de terminar el importe a pagar por el concepto de estaciones...')
 
 valor_pagar = str(input('ingrese el valor a pagar:  '))
 horas = float(input('ingrese la Hora: '))
-precio_por_hora = float(2.5)
+fraccion_hora = float(2.5)
 
-sub_total = horas*precio_por_hora
+sub_total = horas*fraccion_hora
 
 
 
-print('El sueldo a pagar de la hora expresado de '+valor_pagar+' es de: '+str(sub_total)+' DOP por h/mn')
+print('El sueldo a pagar de la hora expresado es '+valor_pagar+' que es: '+str(sub_total)+' DOP por h/mn')
