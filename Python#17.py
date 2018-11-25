0b53598e2cfe44b5dd02cdbc65807280d6ef6745
edman manigat <e.manigat@gmail.com>
2018-11-24 21:32:31 -0400

    Python#17.py

--- Python#17.py
+++ Python#17.py
@@ -1,7 +1,3 @@
 print('este programa imprima cantidad los niños')
-h1=str(input('Entrar el primer ano: '))
-h2=str(input('Entrar el segundo ano: '))
-if h1 > h2:
-    print('el ano de primer es: ',h1,'que es mas grande que',h2)
-else:
-    print('el ano de segundo es: ',h2,'que es mas grande que',h1)
+ 
+17
