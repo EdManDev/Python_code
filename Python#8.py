42ff773d65a2d27b5424c25f5b4f55566ba1ed22
edman manigat <e.manigat@gmail.com>
2018-11-26 19:28:50 -0400

    updte

--- Python#8.py
+++ Python#8.py
@@ -1,11 +1,10 @@
-print("DETREMINE SI ES PAR O en contrario ")
+print("DETREMINE SI ES PAR O EN CONTRARIO IMPARES Y DIVIDE POR 3")
 
 num = int(input("Ingrese Un Valor: "))
 
 if (num % 2) == 0:
-    print('El valor: ',num,' es pares.')
+    print("El valor: ", num, " es pares.")
 else:
-    num=num//3
-    print('El valor: ',num,' es impares (y ya divide por 3).')
-
+    num = num // 3
+    print("El valor: ", num, " es impares (y ya divide por 3 ).")
 
