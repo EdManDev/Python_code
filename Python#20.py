d6c6916f3e2d9017c2d944501284edb41a475215
edman manigat <e.manigat@gmail.com>
2018-11-26 21:18:03 -0400

    updte

--- Python#20.py
+++ Python#20.py
@@ -2,12 +2,16 @@ print("este progama imprima numero positivo o negativo")
 
 
 def generateNumber(num):
+
     mylist = []
+
     for i in range(num + 1):
+
         mylist.append(i)
+
     return mylist
 
-
 x = generateNumber(4)
+
 print(x)
 
