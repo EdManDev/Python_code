96d92fa0df21e3467d6b34b75cfd109843f4578b
edman manigat <e.manigat@gmail.com>
2018-12-03 17:18:13 -0400

    editor

--- Python#9.py
+++ Python#9.py
@@ -1,11 +1,11 @@
-print('calcuo del interes locales')
+print("calcuo del interes locales\n")
 
-tasa_interes=int(input('ingrese la tasa de interes: '))
+tasa_interes = int(input("ingrese la tasa de interes: "))
 
 if tasa_interes < 500:
-    print('la tasa es de 2%')
-elif tasa_interes >= 500:
-    print('la tasa es 5.5%')
+    print("la tasa es de 2%")
+elif tasa_interes == 500:
+    print("la tasa es 5.5%")
 else:
-    print('la tasa es 9%')
+    print("la tasa es 9%")
 
