3e73f4f726d3dd4342a82453bbc96efc7a65cfe4
edman manigat <e.manigat@gmail.com>
2018-12-04 18:03:04 -0400

    .

--- Python#4.1.py
+++ Python#4.1.py
@@ -1,7 +1,7 @@
-horas =  int(input("Ingrese la cantidad de horas : "))
-minutos = int(input("Ingrese la cantidad de minutos : "))
+horas =  int(input("Ingrese la cantidad de horas: "))
+minutos = int(input("Ingrese la cantidad de minutos: "))
 
 fracH = minutos / 60
 tiempo =  fracH + horas
 pago = tiempo * 2.5
-print("El importe a pagar es ", pago, " pesos")
+print("El importe a pagar es", pago, "pesos")
