efa50f3a5a474ab2001bf969c5eb122d9fb04ae1
edman manigat <e.manigat@gmail.com>
2018-12-03 22:29:43 -0400

    updte

---
+++ IF_Elif_Else.py
@@ -0,0 +1,30 @@
+counter = 0
+
+while counter < 3:
+    print("Inside loop")
+    counter = counter + 1
+else:
+    print("Inside else")
+##############################
+
+num = 3
+
+# Try these two variations as well.
+# num = -5
+# num = 0
+
+if num >= 0:
+    print("Positive or Zero")
+else:
+    print("Negative number")
+
+
+#############################
+num = 3.4
+
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")
--- Python#10.py
+++ Python#10.py
@@ -1,3 +1,13 @@
-print('este programa imprima cantidad los niños')
+print("Una tienda que vende pantalones")
 
-10
+pantalones = int(input("ingrese el numero de de cantidad de pantalones: "))
+
+if pantalones == 5:
+    print("\nEsto es el precio Normal\n")
+elif pantalones == 12:
+    print("es el precio de descuento de 15%\n")
+
+elif pantalones > 12:
+    print("es el precio de descuento de 30%\n")
+# else:
+#     print("\nNigun precio han sido selectionado\n")
