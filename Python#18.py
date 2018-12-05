425c18471bc8d5eae515a800c4297287fe88c5c3
edman manigat <e.manigat@gmail.com>
2018-12-05 11:35:38 -0400

    updte

--- Python#18.py
+++ Python#18.py
@@ -1,7 +1,6 @@
 print("programa de calendario de mes")
 
 # calendario=['cero','enero','ferbrero','marzo','april','mayo','junio','julio','agosto','septiembre','octobre','noviembre','deciembre']
-
 # print('el numero que ya selectionado es: 9 = septiembre')
 # print(calendario[9])
 
