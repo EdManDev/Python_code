e114e20a5e88bdd28ef19e9834378eea141e9ff8
edman manigat <e.manigat@gmail.com>
2018-12-05 02:57:00 -0400

    .

--- Python#4.1.py
+++ Python#4.1.py
@@ -1,8 +1,11 @@
-horas =  int(input("Ingrese la cantidad de horas: "))
-minutos = 30 # int(input("Ingrese la cantidad de minutos: "))
+print('de terminar el importe a pagar por el concepto de estaciones...')
+
+
+horas =  int(input('Ingrese la cantidad de horas: '))
+minutos = 30 # int(input('Ingrese la cantidad de minutos: '))
 
 fracH = minutos / 60
 tiempo =  fracH + horas
 pago = tiempo * 2.5
 
-print("El importe a pagar es", pago, "pesos")
+print('El importe a pagar es', pago, 'pesos')
