aca0330c993b4bbc00923eef9ef35aa07b318adb
edman manigat <e.manigat@gmail.com>
2018-12-05 04:01:37 -0400

    editor

--- LOOP.py
+++ LOOP.py
@@ -1,14 +1,14 @@
 print('\x1bc')
 
 # for variable in range(1, 10):
-    # print'this is the section #',variable
+# print'this is the section #',variable
 
- 
-numbers = [2,2,2,2,2,2,2,2,2,2,2,2]
+
+numbers = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 sum = 0
 
 for val in numbers:
-	sum = sum+val
+    sum = sum + val
 
-print("The sum is", sum)
+print('The sum is', sum, 'veses')
--- Python#17.py
+++ Python#17.py
@@ -1,4 +1,10 @@
-print('este programa imprima cantidad los niños')
+print('de un univeso de 20 num cuantas veses este ha sido introducido')
  
-17
-30
+numbers = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+
+sum = 0
+
+for val in numbers:
+    sum = sum + val
+
+print('The sum is', sum, 'veses')
