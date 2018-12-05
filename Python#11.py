b4d72f02d5d0210e558224dde2b8e4b2822382e4
edman manigat <e.manigat@gmail.com>
2018-12-05 11:27:37 -0400

    editor

--- Python#11.py
+++ Python#11.py
@@ -1,4 +1,4 @@
 print("Lea 3 numeros enteros y determine el numero intermedio")
 
 list = [1, 2, 3]
-print(len(list))
+print(enumerate(list))
--- Python#18.py
+++ Python#18.py
@@ -1,16 +1,11 @@
 print("programa de calendario de mes")
 
-# import calendar
-# 
-# y = int(input("ingrese el Ano: "))
-# m = int(input("ingrse el Mes: "))
-# 
-# print(calendar.month(y, m))
+# calendario=['cero','enero','ferbrero','marzo','april','mayo','junio','julio','agosto','septiembre','octobre','noviembre','deciembre']
 
+# print('el numero que ya selectionado es: 9 = septiembre')
+# print(calendario[9])
 
+calendario = ['cero','enero','ferbrero','marzo','april','mayo','junio','julio','agosto','septiembre','octobre','noviembre','deciembre']
 
-
-calendario=['cero','enero','ferbrero','marzo','april','mayo','junio','julio','agosto','septiembre','octobre','noviembre','deciembre']
-
-print('el numero que ya selectionado es: 9 = septiembre')
-print(calendario[9])
+for num, item in enumerate(calendario):
+  print(num, item)
