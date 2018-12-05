cfedcffc70be95bc55fca85642208779312b8e46
edman manigat <e.manigat@gmail.com>
2018-12-05 16:53:54 -0400

    editor

--- Python#22.py
+++ Python#22.py
@@ -1,3 +1,12 @@
-print('este programa imprima cantidad los niños')
+print("en una libreria han puesoto oferta la vena de cuaderno")
 
-22
+compra = int(input("ingrese el valor de compra: "))
+
+if compra == 12:
+    print("regalo niguno")
+elif compra < 24:
+    print("1 - josue para 4 cuaderno")
+elif compra < 36:
+    print("2 - Luis por cada 4 cuaderno")
+elif compra == 36:
+    print("3 - Nelson por cada 4 cuaderno")
