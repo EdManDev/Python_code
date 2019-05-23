625b1bf2275fa3382f1dc286b176e24c66d647a2
TooperTech <ed.manigat@gmail.com>
2019-05-22 21:04:28 -0400

    Create calcular el valor a pagar.py

---
+++ calcular el valor a pagar.py
@@ -0,0 +1,12 @@
+>>> ca=int(input("digite de alfabeto\n"))
+digite de alfabeto
+2
+>>> pa=int(input("digite dede precio\n"))
+digite dede precio
+4
+>>> d=float(input("digite de descuento\n"))
+digite de descuento
+0.1
+>>> f=(ca*pa)-d
+>>> f
+7.9
