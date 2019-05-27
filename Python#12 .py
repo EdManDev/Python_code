10ae67cb99e2841fb5c9cbc1d5e584471f08a2ed
TooperTech <ed.manigat@gmail.com>
2019-05-27 12:23:11 -0400

    Update Python#12 .py

--- Python#12 .py
+++ Python#12 .py
@@ -1,11 +1,10 @@
-print("este programa imprima letra minuscula")
+  print("\n\n este programa va imprimir letra Vocale, semivocales ,o consonante")
 
-a = input("Enter a aaracter: ")
+  a = input("Enter a aaracter: ")
 
-if a == "a" or a == "e" or a == "i" or a == "o" or a == "u":
-    print(a, "es un Vocale")
-elif a == "y":
-    print(a, "es un semivocales")
-else:
-    print(a, "consonante")
-
+  if a == "a" or a == "e" or a == "i" or a == "o" or a == "u":
+      print(a, "es un Vocale")
+  elif a == "y":
+      print(a, "es un semivocales")
+  else:
+      print(a, "consonante")
