c654423ef6b5d02ea029ff7de7512ad3cbea8d9c
TooperTech <ed.manigat@gmail.com>
2019-05-30 10:10:31 -0700

    Update edad y nombre.py

--- edad y nombre.py
+++ edad y nombre.py
@@ -1,8 +1,8 @@
 nombre=(input("Tu Nombre es: "))
 while True:
-  num =int(input("\n\n engrese el valor de tu edad: "))
+  num =int(input("\n engrese el valor de tu edad: "))
 
   if  18 > num:
     print(nombre + " tu edad es menos de 18.")
   else:
-    print("el valor es mayor de 18 te entrego a " +nombre+" 1000 pesos")
+    print(" Tu edad es mayor de 18 te entrego a " +nombre+" 1000 pesos")
