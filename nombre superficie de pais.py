170486e4ffc86a83b59d20fdbbd859e9937c8fe8
TooperTech <ed.manigat@gmail.com>
2019-05-29 20:28:02 -0400

    Create nombre superficie de pais.py

---
+++ nombre superficie de pais.py
@@ -0,0 +1,6 @@
+print('\n\nentrar el superficie de cau lquier Pais:')
+pais=input()
+superficie=input("intrar la superficie:\n")
+densidad=input("intrar la densidad:\n")
+print('\nEl superficie de '+ pais +' es: '+ superficie +'Km y la Densidad es: '+ densidad+' hab.')
+
