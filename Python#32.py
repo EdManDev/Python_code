4a290cad34637c3786427920eb1df149d6a8bf00
TooperTech <ed.manigat@gmail.com>
2019-05-30 14:49:52 -0700

    Update Python#32.py

--- Python#32.py
+++ Python#32.py
@@ -1,8 +1,19 @@
+    
 print('lista vacia que me puedo positionar')
 
 numero = [0,1,2,3,4,5,6,7,8,9,10]
 
-suma = numero[0:6]+numero[6:11] #midle+midle
+suma = numero[1:10]
 
 print(suma)
+
+
+
+# print('lista vacia que me puedo positionar')
+
+# numero = [0,1,2,3,4,5,6,7,8,9,10]
+
+# suma = numero[0:6]+numero[6:11] #midle+midle
+
+# print(suma)
 
