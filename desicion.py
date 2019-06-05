905222a88c9706182d07276f5bad36eb8507300e
EdManDev <ed.manigat@gmail.com>
2019-06-05 10:39:17 -0700

    good

--- desicion.py
+++ desicion.py
@@ -1,13 +1,13 @@
-print('\033c') 
-  or 
-  print('\x1bc')
+print("\033c") or print("\x1bc")
 
 
-print('hello world')
-a=int(input('enter a: '))
-b=int(input('an now enter b: '))
-c=a+b
-print('the result: '+str(c))
+print("this program will aditionate and two Variable an add 2")
+a = int(input("enter a: "))
+b = int(input("an now enter b: "))
 
+c = a + b + 2
 
+print("\nthe result: " + str(c))
+# or
+print("the result: ", c)
 
