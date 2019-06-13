c2d4896864284274844fa4d3df542983a40f903d
EdManDev <ed.manigat@gmail.com>
2019-06-12 20:27:01 -0700

    goahead

--- server_/database.py
+++ server_/database.py
@@ -11,5 +11,15 @@ cursor.execute(sql)
 sql = "USE EdMan ;"
 cursor.execute(sql)
 
-sql=CREATE TABLE IF NOT EXIST owners (id integer NOT NULL AUTO_INCREMENT,)
+sql='''CREATE TABLE IF NOT EXIST owners (id integer NOT NULL AUTO_INCREMENT,
+                                        name varchar(30) NOT NULL,
+                                        gender varchar(7),
+                                        phone varchar(10),
+                                        PRIMARY KEY (id));'''
+cursor.execute(sql)
+sql= 'CREATE TABLE IF NOT EXISTS pets(pet_id integer NOT NULL AUTO_INCREMENT, owner_id integer, name varchar(30),gender varchar varchar(7), species varchar(20),color varchar(10), age integer, PRIMARY KEY (pet_id), FOREIGN KEY (owner_id) REFERNECES owners(id));'
+cursor.execute(sql)
 
+sql="SHOW tables;"
+cursor.execute(sql)
+print(cursor.fetchall())
