aaf74d266b10aba9cedb8ba5c81df95287b9cc0e
EdManDev <ed.manigat@gmail.com>
2019-06-20 22:24:39 -0400

    y

--- work_in_class/school.py
+++ work_in_class/school.py
@@ -1,11 +1,12 @@
 print('\x1bc')
 
 
+name = str(input("\nENTER YOUR NAME: "))
 while True:
-    name = str(input("\nenter your name: "))
-    print("Are you busy", name, "? ")
+    print("-Are you busy", name, "? ")
     response = str(input())
     if response == "yes":
-        print("ok you should take a break")
+        print("-ok you should take a break", name)
     else:
         print("ok good bye", name)
+    name = str(input("\nWELCOME AGAIN ENTER YOUR NAME: "))
