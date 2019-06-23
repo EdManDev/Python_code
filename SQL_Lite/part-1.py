6d489098994a1006d3126e738692a070f679ddef
EdManDev <ed.manigat@gmail.com>
2019-06-23 15:34:28 -0400

    update

---
+++ SQL_Lite/part-1.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqLite3.connct('tutorial.db')
+c = conn.cursor()
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)')
+
+def data_entry():
+    c.execute("INSERT INTO stuffToPlot VALUES(123456, '2019-01-01','Python', 5)")
+    conn.commit()
+    c.close()
+    conn.close()
+
+create_table()
+data_entry()
