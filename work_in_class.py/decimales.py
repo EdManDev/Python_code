b791a4fd6d1e1181f8b3157d04cd74414b2fe43d
EdManDev <ed.manigat@gmail.com>
2019-07-03 17:24:12 -0400

    update

--- work_in_class.py/decimales.py
+++ work_in_class.py/decimales.py
@@ -1,11 +1,11 @@
-# while True:
-#     V=int(input("\nintroduzca el valor: "))
-#     if(V <= 10):
-#         print("El resultado es %.i: "%(V))
-#     elif(V <= 100):
-#         print("El resultado es: %.2f"%(V))
-#     elif(V >= 100):
-#         print("El resultado es: %.3f"%(V))
+while True:
+    V = int(input("\nintroduzca el valor: "))
+    if(V <= 10):
+        print("El resultado es %.i: " % (V))
+    elif(V <= 100):
+        print("El resultado es: %.2f" % (V))
+    elif(V >= 100):
+        print("El resultado es: %.3f" % (V))
 
 ##########################################################
 
@@ -14,7 +14,7 @@
 #     print("terminar")
 # break
 ##########################################################
-v=0
-while (v<5):
-    v=v+1
-    print("valor %.2f",v)
+# v=0
+# while (v<5):
+#     v=v+1
+#     print("valor %.2f",v)
