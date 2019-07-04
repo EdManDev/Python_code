d181fa72c80e35c7689516b2aad1b83526531442
EdManDev <ed.manigat@gmail.com>
2019-07-03 21:09:32 -0400

    update

---
+++ Counter.py
@@ -0,0 +1,10 @@
+print("\033c")
+
+counter = 0
+
+while counter < 3:
+    counter = counter + 1
+    print("Inside loop")
+
+else:
+    print("Inside else")
--- IF_Elif_Else.py
+++ IF_Elif_Else.py
@@ -1,11 +1,3 @@
-counter = 0
-
-while counter < 3:
-    print("Inside loop")
-    counter = counter + 1
-else:
-    print("Inside else")
-##############################
 
 num = 3
 
