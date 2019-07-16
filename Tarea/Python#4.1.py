141885f9095c18a20d70961879bc81aa73307928
EdManDev <ed.manigat@gmail.com>
2019-07-15 21:52:34 -0400

    o

--- Tarea/Python#4.1.py
+++ Tarea/Python#4.1.py
@@ -3,8 +3,8 @@ print("\033c")
 print('en un estasamiento se cobra 2.5 por hora')
 
 horas = int(input('Ingrese la cantidad de horas: '))
-minutos = 30  # int(input('Ingrese la cantidad de minutos: '))
 
+minutos = 30  # int(input('Ingrese la cantidad de minutos: '))
 fract = minutos / 60
 tiempo = fract + horas
 pago = tiempo * 2.5
