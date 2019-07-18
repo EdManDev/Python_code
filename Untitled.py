02e966db1dfc824324ff736ba6ec4ed7b84389d5
Your Name <ed.manigat@gmail.com>
2019-07-17 20:55:10 -0400

    clase de hoy

--- Untitled.py
+++ Untitled.py
@@ -1,7 +1,65 @@
-print('\x1bc')
+# print('\x1bc')
+#  x=0
+#  while x<5:
+#      x=x+1
+#      print(x)
 
-n=5
-sum=0
-i=1
 
-print('this program will use the funcion ')
+# for L in range (1,10,2):
+#   print(L)
+
+
+# for L in range "santiago":
+#   name = name + L
+#   print(name)
+
+# num = int(input("Enter a number: "))
+# if (num % 2) == 0:
+#    print("is Even",(num))
+# else:
+#    print("is Odd",(num))
+
+
+
+# for var2 in range (25,93):
+#   var=var2**3
+#   print(int(var))
+
+
+
+# a = int(input())
+# b = 
+
+
+# print(a**3)
+# print(b**3)
+
+
+
+# numbers = [santiago]
+
+# sum = 0
+
+# for val in numbers:
+#     sum = sum + val
+
+# print('The sum is', sum, 'veses')
+
+
+# n = 10
+# sum = "santiago"
+# i = 1
+
+# while i <= n:
+#     sum = sum + i
+#     i = i+1   
+
+# print("The sum is", sum)
+
+count = 0 
+current = "" 
+for i in "Hello World":
+	count += 1
+	current += i
+	if current == "Hello World!":
+		print(count) 
---
+++ pares de un rango entre 25 y 93.py
@@ -0,0 +1,13 @@
+# var=0
+# for var in range(25,93):
+#     if(var%2==0):
+#         var2=var**3
+#         print(var2)
+#     else:
+#         print("Numero impares", var)
+
+for var in range(25,93):
+  if var():
+    pass
+  else:
+    pass
