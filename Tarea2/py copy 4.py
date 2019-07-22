9ac06ac03700f61ab2cc7355ddd8817802c5e40e
EdMan <mojave@EdMans-MBP.local>
2019-07-21 22:08:01 -0400

    #4

--- Tarea2/py copy 4.py
+++ Tarea2/py copy 4.py
@@ -1,4 +1,16 @@
 print("\033c")
 
 
-print("Raice ((a+b)(a*b))2")
+print("la Raice ((a+b)(a*b)2)")
+a = 4
+b = 2
+c = a + 2
+
+x = a + b
+y = a * b
+z = x * y * 2
+
+sqrt = z ** 0.5
+
+print("El resultado del equacion es: ", int(sqrt))
+
