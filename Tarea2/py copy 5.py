2b319e2bde65617fece555965b483526b7105dab
EdMan <mojave@EdMans-MBP.local>
2019-07-22 00:18:30 -0400

    14 & 5  & Average

--- Tarea2/py copy 5.py
+++ Tarea2/py copy 5.py
@@ -1,1 +1,14 @@
 print("\033c")
+print("\nEste programa va imprimir letra Vocale , semivocales o,consonante")
+
+while True:
+  a = input("\n\n Enter a un caracter: ")
+
+  if a == "a" or a == "e" or a == "i" or a == "o" or a == "u":
+      print(a, "es un Vocale")
+  elif a == "y" or "Y":
+      print(a, "es un semivocales")
+  elif a:
+      print(a, "es un consonate")
+  else:
+      print(a, "N/A")
---
+++ promedio.py
@@ -0,0 +1,23 @@
+print("\x1bc")
+# import statistics
+
+# promedio = [4, 12]
+
+# x = statistics.mean(promedio)
+
+# print("El promedio entre", promedio, "es :", int(x))
+
+
+import statistics
+
+# list of positive integer numbers
+data1 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+data2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+x = statistics.mean(data1)
+y = statistics.mean(data2)
+
+# Printing the mean
+print("Mean 1 is :", x)
+print("Mean 2 is :", y)
+
