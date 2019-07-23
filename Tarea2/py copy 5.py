0707cea0197bfb30672375b825c56fa0dab9cab5
EdMan <mojave@EdMans-MBP.local>
2019-07-23 13:44:32 -0400

    updt5

--- Tarea2/py copy 5.py
+++ Tarea2/py copy 5.py
@@ -2,13 +2,17 @@ print("\033c")
 print("\nEste programa va imprimir letra Vocale , semivocales o,consonante")
 
 while True:
-  a = input("\n\n Enter a un caracter: ")
+    caracter = input("\n\n entrar  un caracter: ")
 
-  if a == "a" or a == "e" or a == "i" or a == "o" or a == "u":
-      print(a, "es un Vocale")
-  elif a == "y" or "Y":
-      print(a, "es un semivocales")
-  elif a:
-      print(a, "es un consonate")
-  else:
-      print(a, "N/A")
+    if (
+        caracter == "a"
+        or caracter == "e"
+        or caracter == "i"
+        or caracter == "o"
+        or caracter == "u"
+    ):
+        print(caracter, "es un Vocale")
+    elif caracter == "y":
+        print(caracter, "es un semivocales")
+    else:
+        print(caracter, "es un consonate")
