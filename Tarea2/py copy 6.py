f048c1e3872cec9b66c2e0973d399ed14f9b7003
EdManDev <ed.manigat@gmail.com>
2019-08-01 15:53:05 -0400

    updt

--- Tarea2/py copy 6.py
+++ Tarea2/py copy 6.py
@@ -1,12 +1,12 @@
 print("\033c")
 print("este programa va determinar el importe a pagar por concepto de estacionamiento")
 
-nombre = str(input('ingrese el nombre del impleado: '))
-horas = float(input('ingrese la hora de trabajadores: '))
-precio_por_hora = float(input('ingrese la hora del trabajado: '))
+hora = int(input("digite horas de a: "))
+# precio_hora = float(input("digite precio por hora: "))
+precio_hora = float(input("digite precio por hora: "))
+ars = (hora * precio_hora) * 0.02
+afp = (hora * precio_hora) * 0.0304
+sb = hora * precio_hora
+resultado = sb - ars - afp
+print("el importe a pagar por el concepto de estacionamiento es: ", resultado)
 
-sub_total = horas*precio_por_hora
-descuento = sub_total*0.07
-sueldo = sub_total-descuento
-
-print('El sueldo de los impleado que es ' +nombre+' es de: '+str(sueldo))
