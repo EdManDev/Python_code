ad64f10cf910ae55d018f46f7b86c01f4adfbbfa
EdMan <mojave@EdMans-MBP.local>
2019-08-01 15:50:58 -0400

    update now

---
+++ work_in_class.py/Python List append().py
@@ -0,0 +1,112 @@
+# animal list
+animal = ["cat", "dog", "rabbit"]
+
+# an element is added
+animal.append("guinea pig")
+
+# Updated Animal List
+print("Updated animal list: ", animal)
+
+
+########################################################
+
+# animal list
+animal = ["cat", "dog", "rabbit"]
+
+# another list of wild animals
+wild_animal = ["tiger", "fox"]
+
+# adding wild_animal list to animal list
+animal.append(wild_animal)
+
+# Updated List
+print("Updated animal list: ", animal)
+
+########################## INSERT ##############################
+# vowel list
+vowel = ["a", "e", "i", "u"]
+
+# inserting element to list at 4th position
+vowel.insert(3, "o")
+
+print("Updated List: ", vowel)
+
+########################################################
+mixed_list = [{1, 2}, [5, 6, 7]]
+
+# number tuple
+number_tuple = (3, 4)
+
+# inserting tuple to the list
+mixed_list.insert(1, number_tuple)
+
+print("Updated List: ", mixed_list)
+
+######################### REMOVE ###############################
+# animal list
+animal = ["cat", "dog", "rabbit", "guinea pig"]
+
+# 'rabbit' element is removed
+animal.remove("rabbit")
+
+# Updated Animal List
+print("Updated animal list: ", animal)
+
+########################################################
+
+# If a list contains duplicate elements
+# the remove() method removes only the first instance
+
+# animal list
+animal = ["cat", "dog", "dog", "guinea pig", "dog"]
+
+# 'dog' element is removed
+animal.remove("dog")
+
+# Updated Animal List
+print("Updated animal list: ", animal)
+
+########################################################
+
+# animal list
+animal = ["cat", "dog", "rabbit", "guinea pig"]
+
+# Deleting 'fish' element
+animal.remove("fish")
+
+# Updated Animal List
+print("Updated animal list: ", animal)
+
+########################### COUNT #############################
+
+# vowels list
+vowels = ["a", "e", "i", "o", "i", "u"]
+
+# count element 'i'
+count = vowels.count("i")
+
+# print count
+print("The count of i is:", count)
+
+# count element 'p'
+count = vowels.count("p")
+
+# print count
+print("The count of p is:", count)
+
+########################################################
+
+# random list
+random = ["a", ("a", "b"), ("a", "b"), [3, 4]]
+
+# count element ('a', 'b')
+count = random.count(("a", "b"))
+
+# print count
+print("The count of ('a', 'b') is:", count)
+
+# count element [3, 4]
+count = random.count([3, 4])
+
+# print count
+print("The count of [3, 4] is:", count)
