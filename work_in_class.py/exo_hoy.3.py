f4ce5e180d2e0854684a5ce076f0ab3b22791d73
EdMan <mojave@EdMans-MBP.local>
2019-08-01 20:23:36 -0400

    format

--- work_in_class.py/exo_hoy.3.py
+++ work_in_class.py/exo_hoy.3.py
@@ -1,15 +1,15 @@
-# print("\033c") 
+# print("\033c")
 
 compra = int(input("ingrese el valor del precio: "))
 meses = input("ingrese el meses")
 
-if compra ==  3:   
+if compra == 3:
     print("Diciembre 3%")
 elif compra == 5:
-   print("octubre 5.5%")
+    print("octubre 5.5%")
 elif compra == 4:
     print("Noviembre 4%")
 elif compra == 8:
     print("septiembre 8%")
 else:
-  print("N/A")
+    print("N/A")
