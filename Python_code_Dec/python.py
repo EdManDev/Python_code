afd6eaa0f330a548a8de41f1d6e83d49ec2a75dd
edmandev <ed.manigat@gmail.com>
2019-11-28 19:51:17 -0400

    ok

---
+++ Python_code_Dec/python.py
@@ -0,0 +1,25 @@
+print("\033c")
+print("hello world")
+
+# y=8
+# while(y<=20):
+#     y=y+2
+#     if%2
+
+# D=float(input("digite al limite: "))
+# C=0
+# while (C<=D):
+# # for C in range (D):
+#   C=C+1
+#   if ((C%2)==0):
+#     print("Pares", C)
+#   else:
+#     print("impares",C)
+#   break
+
+##############################
+
+t=(2,4,5,6)
+A=t*2
+B=t[2]
+print(B)
