05305acb5fef2cfa506591c880cb13530ecdd5c3
edmandev <ed.manigat@gmail.com>
2019-12-11 22:33:50 -0400

    add

--- Tarea3/py copy 3.py
+++ Tarea3/py copy 3.py
@@ -2,7 +2,7 @@ print("\033c")
 print("al usuario su edad y muestre por pantalla si es mayor de edad o no")
 edad = int(input("su edad?: "))
 
-if(edad > 1):
+if(edad > 18):
     print("mayor de edad: ")
 else:
     print("menor de edad: ")
